bbe0eda5a9b19cad521695d70d188b363588ac53
xdeng10 <60670592+xdeng10@users.noreply.github.com>
2021-01-18 00:51:53 -0500

    allow remote connection for mysql

--- includes/connectDB.php
+++ includes/connectDB.php
@@ -1,8 +1,15 @@
 <?php
+//Local connection
+/*
 $db_servername="localhost";
 $db_username="root";
 $db_password="";
 $db_name="shopify2021_imagerepo";
+*/
+$db_servername="lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_username="c8dc8h2u94iz4o29";
+$db_password="gq48kizwlxljqfmf";
+$db_name="uymlztae5kse83h8";
 
 $conn=mysqli_connect($db_servername,$db_username,$db_password,$db_name);
 
